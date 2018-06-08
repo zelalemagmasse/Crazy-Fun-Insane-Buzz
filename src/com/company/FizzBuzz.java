2eb618e81a3f77a6b7b6410f9468135c2ff0a92a
zelalemagmasse <zelalem.agmasse@gmail.com>
2018-06-08 09:27:04 -0400

    Shazam verion of FizzBuzz

--- src/com/company/FizzBuzz.java
+++ src/com/company/FizzBuzz.java
@@ -1,31 +1,66 @@
 package com.company;
-
+import java.util.Random;
 import java.util.ArrayList;
 
 public class FizzBuzz {
+
+    private int smallNum;
+    private int bigNum;
+
+    public int getSmallNum() {
+        return smallNum;
+    }
+
+    public int getBigNum() {
+        return bigNum;
+    }
+
     public ArrayList fizBuzDisplay(){
         ArrayList fizBuzCount = new ArrayList();
-        for(int i=1;i<=100;i++){
-            if(i%3==0){
-                if(i%5==0){
-                    fizBuzCount.add("FizzBuzz");
-                }
-                else
-                    fizBuzCount.add("Fizz");
+        Random bigEnd=new Random();
 
+        int bign=1 + bigEnd.nextInt(99);
+        int endn=1 + bigEnd.nextInt(99);
+
+
+
+        smallNum=intSmallerComparator(bigNum,endn);
+        bigNum=intGreaterComparator(bign,endn);
+
+        if(smallNum==bigNum){
+            fizBuzCount.add("Shazam");
+        }
+        else {
+
+            for (int i = smallNum; i <= bigNum; i++) {
+                if (i % 3 == 0) {
+                    if (i % 5 == 0) {
+                        fizBuzCount.add("FizzBuzz");
+                    } else
+                        fizBuzCount.add("Fizz");
+
+                } else if (i % 5 == 0) {
+                    if (i % 3 == 0) {
+                        fizBuzCount.add("FizzBuzz");
+                    } else
+
+                        fizBuzCount.add("Buzz");
+                } else
+
+                    fizBuzCount.add(i);
             }
-            else if (i%5==0){
-                if(i%3==0){
-                    fizBuzCount.add("FizzBuzz");
-                }
-                else
-
-                    fizBuzCount.add("Buzz");
-            }
-            else
-
-                fizBuzCount.add(i);
         }
         return fizBuzCount;
+    }
+
+    private int intGreaterComparator(int randNum1,int randNum2){
+        if(randNum1>randNum2)
+            return randNum1;
+        else return randNum2;
+    }
+    private int intSmallerComparator(int randNum1,int randNum2){
+        if(randNum1>randNum2)
+            return randNum2;
+        else return randNum1;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,18 @@
 package com.company;
 
+
+
 public class Main {
 
 
     public static void main(String[] args) {
-
         FizzBuzz fizzBuzzPointer=new FizzBuzz();
-        for(int i=0;i<fizzBuzzPointer.fizBuzDisplay().size();i++){
-            System.out.println(fizzBuzzPointer.fizBuzDisplay().get(i));
-        }
+        int startNum=fizzBuzzPointer.getSmallNum();
+        int endNum=fizzBuzzPointer.getBigNum();
 
+        for(Object retriv: fizzBuzzPointer.fizBuzDisplay() ) {
+            System.out.println(retriv);
+        }
 
     }
 }
